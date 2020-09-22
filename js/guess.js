19eb91a00dcea3133d3b6c37631da40a7233ae65
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-22 18:06:53 +0200

    Jeu terminé

--- js/guess.js
+++ js/guess.js
@@ -1,10 +1,39 @@
 let form = document.querySelector("form");
 let input = document.getElementById("inputGuess");
 let resultat = document.getElementById("resultat");
+let boutonRestart = document.getElementById("restart");
+let essai = 1;
+let random = Math.floor(Math.random() * 100);
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
-    let inputGuess = input.value;
-    resultat.textContent = "Vous avez tapé " + inputGuess;
+    let inputGuess = Number(input.value);
     input.value = "";
+    if (inputGuess === random) {
+        resultat.style.color = "var(--green)";
+        resultat.textContent = "Essai n° " + essai + " : " + inputGuess + " ! Bravo, vous avez trouvé !";
+        boutonRestart.style.opacity = "1";
+        boutonRestart.style.cursor = "pointer";
+    } else {
+        if (essai > 10) {
+            resultat.style.color = "var(--red)";
+            resultat.textContent = "Essai n° " + essai + " : " + inputGuess + " : GAME OVER";
+        } else {
+            if (inputGuess < random) {
+                resultat.style.color = "var(--red)";
+                resultat.textContent = "Essai n° " + essai + " : " + inputGuess + " : C'est plus !";
+                essai++;
+            } else {
+                resultat.style.color = "var(--red)";
+                resultat.textContent = "Essai n° " + essai + " : " + inputGuess + " : C'est moins !";
+                essai++;
+            }
+            
+        }
+    }
+})
+
+boutonRestart.addEventListener("click", function(e) {
+    e.preventDefault();
+    window.location.reload();
 })
